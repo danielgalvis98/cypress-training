ca3b670c3a78a2bf5a38fe8b7f5970de2289a9d7
Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
2021-07-15 23:19:42 -0500

    filling form (#14)
    * filling form
    
    * Add EOF

---
+++ cypress/interfaces/IPersonalInformation.ts
@@ -0,0 +1,11 @@
+export interface IPersonalInformation {
+    name: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    mobileNumber: string,
+    hobbies: string[];
+    currentAddress: string;
+    state: string;
+    city: string;
+}
