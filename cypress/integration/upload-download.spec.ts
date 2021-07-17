3168f164c114083a936a8c623eb5d6efdb3699c0
Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>
2021-07-17 13:26:56 -0500

    Verify downloads (#16)

--- cypress/integration/upload-download.spec.ts
+++ cypress/integration/upload-download.spec.ts
@@ -1,15 +1,30 @@
-import { UploadPage } from "../page";
+import { DownloadPage, UploadPage } from "../page";
 
 describe("The user navigates to the upload page", () => {
 
     const uploadPage = new UploadPage();
 
     it("shoould be able to upload a file", () => {
-        const fileName = 'pablo.jpg'
+        const fileName = "pablo.jpg"
         uploadPage.visitPage()
 
         uploadPage.uploadFile(fileName)
 
         uploadPage.verifyTitle(fileName)
+    });
+});
+
+describe("The user navigates to the download page", () => {
+
+    const downloadPage = new DownloadPage();
+
+    it("should be able to download a file", () => {
+        const textContent = "This is a test text";
+        downloadPage.visitPage()
+
+        downloadPage.generateData(textContent)
+        downloadPage.downloadTextFile()
+
+        downloadPage.verifyDownload(textContent)
     });
 });
